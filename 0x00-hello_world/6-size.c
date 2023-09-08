b4089dbba62b718821af18e73d070596db50de97
kyrolles <kyrollesraafat@gmail.com>
2023-09-08 05:47:27 +0300

    angryy

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %lu ", sizeof(char));
-printf("Size of an int: %lu ", sizeof(int));
-printf("Size of a long int: %lu ", sizeof(long int));
-printf("Size of a long long: %lu ", sizeof(long long));
-printf("Size of a float: %lu ", sizeof(float));
+printf("Size of a char: %lu \n", sizeof(char));
+printf("Size of an int: %lu \n", sizeof(int));
+printf("Size of a long int: %lu \n", sizeof(long int));
+printf("Size of a long long: %lu \n", sizeof(long long));
+printf("Size of a float: %lu \n", sizeof(float));
 return (0);
 }

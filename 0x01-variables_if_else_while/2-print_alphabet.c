8b2be7c9a138441b323fa89b7efe55fcb9762404
kyrolles <kyrollesraafat@gmail.com>
2023-09-12 04:11:34 +0300

    ha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
- * Description: A C  program will assign a random number to the variable n each
- * time it is executed order to print whether the number stored in the variable
- * n is positive or negative.
+ * Description: A.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n < 0)
+char ch;
+while (ch <= 'z')
 {
-printf("%d is negative\n", n);
+putchar(ch);
+ch++;
 }
-else if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
+putchar('\n');
 return (0);
 }

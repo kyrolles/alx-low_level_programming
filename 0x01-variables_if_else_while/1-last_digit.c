6f0fee6f5aaa43753e8b57f64ad3d37a261e9ff9
kyrolles <kyrollesraafat@gmail.com>
2023-09-12 03:47:01 +0300

    nnn

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ if (k > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, k);
 }
-else if (k < 6)
+else if (k < 6 && k != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
 }

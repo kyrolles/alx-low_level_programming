a0d4b480a86ae9c27b41ee466e97292a7fa36701
kyrolles <kyrollesraafat@gmail.com>
2023-09-12 04:50:56 +0300

    hmmmm

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int ch = 0;
+while (ch < 10)
+{
+printf("%d", ch);
+ch++;
+}
+printf("\n");
+return (0);
+}

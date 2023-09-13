bed169b929b56f29dc3dc6f88c65f14a393b1881
kyrolles <kyrollesraafat@gmail.com>
2023-09-14 02:23:46 +0300

    3+

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,21 @@
 #include"main.h"
 
 /**
- * islower - the main fuction
+ * _islower - the main fuction
  *
  * @c : the comming symbole
  *
- * */
+ * return - 0 or 1 (sucess)
+ */
 
 int _islower(int c)
 {
-if ( c >= 97 && c <= 122 )
+if (c >= 97 && c <= 122)
 {
-return 1;
+return (1);
 }
 else
 {
-return 0;
+return (0);
 }
 }

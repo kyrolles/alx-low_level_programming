9fb6a00f980ad40941b595441aa6dc32993cad63
kyrolles <kyrollesraafat@gmail.com>
2023-09-14 03:12:28 +0300

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_last_digit - the main fuction
+ *
+ * @n : the comming symbole
+ *
+ * Return: 0 it will return not _islower
+ * or 1  it will return _islower (sucess)
+ *
+ */
+
+int print_last_digit(int n)
+{
+return (n % 10);
+}

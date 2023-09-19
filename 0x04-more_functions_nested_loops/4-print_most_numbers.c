1780c7acc279f9f07e18fea0179e69a7170d21a6
kyrolles <kyrollesraafat@gmail.com>
2023-09-19 21:02:21 +0300

    Task 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * expect 2 or 4, followed by a new line.
+ *
+ * Return: nothing.
+ */
+
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+if (i == 50 || i == 52)
+{
+continue;
+}
+else
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}

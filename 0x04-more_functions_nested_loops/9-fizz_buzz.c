eb0c0a7ba91c6bf0511acd7d5a730af790f73081
kyrolles <kyrollesraafat@gmail.com>
2023-09-20 08:33:50 +0300

    4 modify Task FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,27 +14,22 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
-{
-if (i == 100)
 {
 printf("FizzBuzz");
 }
 else
 {
-printf("FizzBuzz ");
+printf("%d", i);
 }
-}
-else
-{
-printf("%d ", i);
-}
+if (i < 100)
+printf(" ");
 }
 printf("\n");
 return (0);

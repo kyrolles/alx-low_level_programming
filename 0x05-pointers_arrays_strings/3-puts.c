eb27b549cde5f22a0b8d16d210ef1df79fdb33f2
kyrolles <kyrollesraafat@gmail.com>
2023-09-29 00:43:13 +0300

    Task 3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+  * _strlen -  prints a string, followed by a new line, to stdout
+  * @str: the string.
+  *
+  */
+
+void _puts(char *str)
+{
+_putchar(*str);
+_putchar('\n');
+
+}

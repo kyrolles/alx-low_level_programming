fe3a870a1ba289920d2e0f296d723135b69501b0
kyrolles <kyrollesraafat@gmail.com>
2023-09-29 04:45:17 +0300

    2 modify 6. Half the lies they tell about me arenot true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,15 +9,15 @@
 void puts2(char *str)
 {
 int i = 0;
-while(str[i]) 
+while (str[i])
 {
 if (i == 0)
 {
 _putchar(str[i]);
 }
-else if (i%2 == 0)
+else if (i % 2 == 0)
 {
- _putchar(str[i]);  
+_putchar(str[i]);
 }
 i++;
 }

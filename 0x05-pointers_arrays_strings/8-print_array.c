662569dbd9a13be6381a44f1cf19080cd85c1711
kyrolles <kyrollesraafat@gmail.com>
2023-09-29 06:05:58 +0300

    Task 8. Arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+  * print_array -  prints half of a string, followed by a new line.
+  * @a: the array.
+  * @n: the number of arry;
+  *
+  */
+
+void print_array(int *a, int n)
+{
+int x, i;
+
+n = x;
+while (x--)
+{
+_putchar(a[i]);
+_putchar(',');
+_putchar(' ');
+i++;
+}
+_putchar('\n');
+}

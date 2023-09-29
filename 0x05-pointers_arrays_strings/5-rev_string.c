5f7035f6975fef9810faf683fbd1282bb6bd2093
kyrolles <kyrollesraafat@gmail.com>
2023-09-29 03:13:33 +0300

    5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+  * rev_string -  prints a string, followed by a new line, to stdout
+  * @s : pointer.
+  *
+  */
+
+void rev_string(char *s)
+{
+int end = 0;
+
+while (*s != '\0')
+{
+_putchar(*s);
+s++;
+}
+while (*(s + end) != '\0')
+{
+end++;
+}
+while (end--)
+{
+_putchar(s[end]);
+}
+_putchar('\n');
+}

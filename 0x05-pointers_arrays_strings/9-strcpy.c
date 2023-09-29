7427020925455c86842c51bfa1309fbc1c3732c4
kyrolles <kyrollesraafat@gmail.com>
2023-09-29 10:52:02 +0300

    5 modify Task 9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = -1;
 
-	while (*src != '\0')
-	{
-		index++;
-		dest[index] = *(src + index);
-	}
+do {
+index++;
+dest[index] = *(src + index);
+} while (*src != '\0');
 	return (dest);
 }
 

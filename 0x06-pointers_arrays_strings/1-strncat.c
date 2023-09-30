c943cddd6cc3221ada5c6ccd65ed798611bd0898
kyrolles <kyrollesraafat@gmail.com>
2023-10-01 01:34:03 +0300

    Task 1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncat - to add to string to each other with n int.
+ * @dest: first string.
+ * @src: second string.
+ * @n: the num i want to add.
+ *
+ * Return: first string (dest).
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int countd, counts;
+int i = 0;
+
+for (countd = 0; *(dest + countd) != '\0'; countd++)
+{
+}
+for (counts = 0; *(dest + counts) != '\0'; counts++)
+{
+}
+while (n--)
+{
+dest[countd] = src[i];
+i++;
+countd++;
+}
+return (dest);
+}

8ffa4e1606017234fe8cf1499205dbd9a3015412
kyrolles <kyrollesraafat@gmail.com>
2023-10-02 13:12:21 +0300

    2 modify 2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,12 @@ char *_strchr(char *s, char c)
 {
 unsigned int i;
 
-for (i = 0; *s != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == c)
 {
 return (s + i);
 }
 }
-return (s + i);
 return (NULL);
 }

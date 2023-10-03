07ee8cc4281d1b1e71b5e64f45c07f606345b984
kyrolles <kyrollesraafat@gmail.com>
2023-10-03 03:17:57 +0300

    8 modify Task 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,21 +10,21 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i;
-	unsigned int j;
-	char *p;
+unsigned int i;
+unsigned int j;
+char *p;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				 p = &s[i];
-				return (p);
-			}
-		}
-	}
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+p = &s[i];
+return (p);
+}
+}
+}
 
-	return (NULL);
+return (NULL);
 }

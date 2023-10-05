1f6de33c27399fa5857e19fee646b7b84016b443
kyrolles <kyrollesraafat@gmail.com>
2023-10-05 13:44:08 +0300

    8 modify 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,37 +9,40 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *ptr;
-unsigned int i, k;
-unsigned int size1, size2, size;
+	char *ptr;
+	unsigned int i, k, size1 = 0, size2 = 0, size;
 
-if (s1 != NULL)
-{
-for (size1 = 0; s1[size1] != '\0'; size1++)
-{
-}
-}
-if (s2 != NULL)
-{
-for (size2 = 0; s2[size2] != '\0'; size2++)
-{
-}
-}
-size = size1 + size2;
-size += 1;
-ptr = (char *) malloc(sizeof(char) * size);
-if (ptr == NULL)
-{
-return (NULL);
-}
-for (i = 0; i < size1; i++)
-{
-*(ptr + i) = *(s1 + i);
-}
-for (k = 0; k < size2;  k++)
-{
-*(ptr + k + size1) = *(s2 + k);
-}
- ptr[size - 1] = '\0';
-return (ptr);
+	if (s1 != NULL)
+	{
+		for (size1 = 0; s1[size1] != '\0'; size1++)
+		{
+		}
+	}
+	if (s2 != NULL)
+	{
+		for (size2 = 0; s2[size2] != '\0'; size2++)
+		{
+		}
+	}
+	size = size1 + size2 + 1;
+	ptr = (char *) malloc(sizeof(char) * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+
+	for (k = 0; k < size2; k++)
+	{
+		ptr[size1 + k] = s2[k];
+	}
+
+	ptr[size - 1] = '\0'; /* null-terminate the concatenated string */
+
+	return (ptr);
 }

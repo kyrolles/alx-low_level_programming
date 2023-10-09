901f688dc8a62dbcc5270c461fbd99cdc4489411
kyrolles <kyrollesraafat@gmail.com>
2023-10-09 12:34:09 +0300

    0 modify Task 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
-
 /**
  * *string_nconcat - function that allocates memory using malloc.
  * @s1: the first string.
@@ -14,40 +12,38 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *ptr;
 int i = 0, x, r = 0, o;
-int size1 = strlen(s1);
-int size2 = strlen(s2);
+int size1;
+int size2;
 
-ptr = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+if (s1 == NULL)
+{s1 = ""; }
+if (s2 == NULL)
+{s2 = ""; }
+for (size1 = 0; s1[size1] != '\0'; size1++)
+{}
+for (size2 = 0; s1[size2] != '\0'; size2++)
+{}
+ptr = (char *)malloc(size1 + size2 + 1);
 if (ptr == NULL)
 {
-return (NULL);
-}
+return (NULL); }
 if (n >= size1)
-{
-x = size1;
-}
+{x = size1; }
 else
-{
-x = n;
-}
+{x = n; }
 while (x--)
 {
 ptr[r] = s1[r];
 r++;
 }
 if (n >= size2)
-{
-o = size2;
-}
+{o = size2 + 1; }
 else
-{
-o = n;
-}
+{o = n; }
 while (o--)
 {
 ptr[r + i] = s2[i];
 i++;
 }
-
 return (ptr);
 }

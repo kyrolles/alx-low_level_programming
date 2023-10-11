fb103eb71828b1dfcaa695d5aaf43727b949cbc8
kyrolles <kyrollesraafat@gmail.com>
2023-10-11 03:49:18 +0300

    Task 0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - a dog struct.
+ * @name:dog's name.
+ * @age: dog's age.
+ * @owner: owner's name.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif

783f3bc8794e254d615ad2af097836d879d52788
kyrolles <kyrollesraafat@gmail.com>
2023-10-14 06:44:51 +0300

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Write a function that returns the sum of all its parameters.
+ * @n: is the number of integers passed to the function.
+ * @separator: is the string to be printed between numbers.
+ * Return: Nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list arg;
+unsigned int i, x = 0;
+
+
+
+va_start(arg, n);
+
+for (i = 0; i < n; i++)
+{
+x = va_arg(arg, int);
+if (separator != NULL)
+{
+printf("%d", x);
+}
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+va_end(arg);
+}

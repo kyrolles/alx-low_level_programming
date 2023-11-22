89c7413cdf47f24c4a210b4c502bb0e4ce367d1b
kyrolles <kyrollesraafat@gmail.com>
2023-11-22 23:09:29 +0200

    not again please

---
+++ redo0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that convert binary_to_unsigned int;
+ * @b:the array.
+ *
+ * Return: 98 if fails & value inside ptr if true.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+
+
+return (0);
+}

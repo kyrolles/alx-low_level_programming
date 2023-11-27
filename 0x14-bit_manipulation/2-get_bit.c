a7c031be40816891ec594dd1bc6494155f80e164
kyrolles <kyrollesraafat@gmail.com>
2023-11-27 22:19:30 +0200

    ha ha ah

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
+
 /**
- * binary_to_uint - function that convert binary_to_unsigned int;
- * @b:the array.
+ * get_bit - function that make you see what this bit containe.
+ * @n:the number.
+ * @index:the the position.
  *
  * Return: 98 if fails & value inside ptr if true.
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
+int bit;
 
-
-
-return (0);
+bit = ((n >> index) & 1);
+	if (index > 64)
+		return (-1);
+	return (bit);
 }

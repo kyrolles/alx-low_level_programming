43fb711ef08afe0bc2765a36d8698f6b39083eff
kyrolles <kyrollesraafat@gmail.com>
2024-02-26 18:42:23 +0200

    solve 1-creat_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,14 +1,43 @@
-#include"main.h" 
-#include<unistd.h>
-#include<fcntl.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
 /**
- * binary_to_uint - function that convert binary_to_unsigned int;
- * @b:the array.
+ * append_text_to_file - append text to the end of a file
+ * @filename: name of file to write into.
+ * @text_content: text to append to file
  *
- * Return: 98 if fails & value inside ptr if true.
+ * Return: 1 (SUCCESS), else -1 (FAILURE)
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
+int fd;
+int chars_written;
+
+	fd = chars_written = 0;
+	if (!filename)
+	{
+		return (-1);
+	}
+	else if (!text_content || !text_content[0])
+	{
+		return (1);
+	}
+fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd < 0)
+			{return (-1); }
+
+chars_written = write(fd, text_content, _strlen(text_content));
+	if (chars_written < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
 
 	return (0);
 }

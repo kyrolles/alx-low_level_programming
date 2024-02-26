a829d3843f8820d7a78fe8a28caf8c6c6870537a
kyrolles <kyrollesraafat@gmail.com>
2024-02-26 17:53:33 +0200

    solve 1-creat_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,13 +24,12 @@ fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		if (fd < 0)
 			{return (-1); }
 
-chars_written = write(fd, text_content, strlen(text_content));
+chars_written = write(fd, text_content, strlen(text_content)+1);
 	if (chars_written < 0)
 	{
 		close(fd);
 		return (-1);
 	}
-
 
 	close(fd);
 	return (1);

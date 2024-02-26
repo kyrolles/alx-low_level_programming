4f62267e1d1a3feac68e8a11ee141492be26cf0e
kyrolles <kyrollesraafat@gmail.com>
2024-02-26 17:43:22 +0200

    solve 1-creat_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,18 +14,15 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-/* char *buf; */
 int chars_written;
 
 	if (!filename)
 	{
 		return (-1);
 	}
-fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0600);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		if (fd < 0)
 			{return (-1); }
-
-/* buf = malloc(sizeof(char)* strlen(text_content)); */
 
 chars_written = write(fd, text_content, strlen(text_content));
 	if (chars_written < 0)

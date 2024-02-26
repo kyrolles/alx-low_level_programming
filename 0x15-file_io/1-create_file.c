a884fab167aee536333d436b87d9c4f026fe4c7c
kyrolles <kyrollesraafat@gmail.com>
2024-02-26 17:34:56 +0200

    solve 1-creat_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,15 +1,40 @@
-#include"main.h" 
-#include<unistd.h>
-#include<fcntl.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
 /**
- * binary_to_uint - function that convert binary_to_unsigned int;
- * @b:the array.
+ * create_file - function that will create a new file and fill with with some
+ * content.
+ * @filename: name of the file to create.
+ * @text_content: text to add to the new file.
  *
- * Return: 98 if fails & value inside ptr if true.
+ * Return: Always 1 on scucess, -1 on Failure
  */
 int create_file(const char *filename, char *text_content)
 {
+int fd;
+/* char *buf; */
+int chars_written;
 
-	return (0);
+	if (!filename)
+	{
+		return (-1);
+	}
+fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0600);
+		if (fd < 0)
+			{return (-1); }
+
+/* buf = malloc(sizeof(char)* strlen(text_content)); */
+
+chars_written = write(fd, text_content, strlen(text_content));
+	if (chars_written < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+
+
+	close(fd);
+	return (1);
 }
-
